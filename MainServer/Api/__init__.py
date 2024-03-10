21d2fed5730f985558fe18572da254d6d49e1a91
lolovlad <lolovlad33@gmail.com>
2024-03-10 19:36:31 +0400

    grps -> rest api

--- MainServer/Api/__init__.py
+++ MainServer/Api/__init__.py
@@ -3,11 +3,13 @@ from .settings import router as router_settings
 from .task_file_settings_test import router as router_task_file_settings_test
 from .compiler import router as router_compiler
 from .answer import router as router_answer
+from .contest import router as router_contest
 
 router = APIRouter(prefix="/v1")
 router.include_router(router_settings)
 router.include_router(router_task_file_settings_test)
 router.include_router(router_compiler)
 router.include_router(router_answer)
+router.include_router(router_contest)
 
 
